be5a58b621691eab47e910e66065d03e2029e8d6
Gzie3009 <108942030+Gzie3009@users.noreply.github.com>
2023-04-06 03:23:32 +0530

    CarMania-backend

---
+++ app.js
@@ -0,0 +1,357 @@
+const express=require("express")
+const mongoose = require("mongoose")
+const emailValidator =require("email-validator")
+const jwt= require("jsonwebtoken")
+const JWT_KEY="abcdefghijklmnopqrstuvwxyz"
+const cookieParser=require("cookie-parser")
+const bodyParser=require("body-parser")
+const cors=require("cors")
+const bcrypt=require("bcrypt")
+
+const app= express()
+const allowedOrigins = ['http://localhost:3000']; // list of allowed client origins
+
+app.use(cors({
+  credentials: true,
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+
+mongoose.connect("mongodb+srv://Gzie3009:saikiaraj112233@cluster1.vitgru9.mongodb.net/users?retryWrites=true&w=majority").then(
+    function(){
+        console.log("db connected")
+    }
+).catch((e)=>{
+    console.log(e)
+})
+
+
+
+const userSchema=mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        validate:function(){
+            return(emailValidator.validate(this.email))
+        }
+    },
+    phone:{
+        type:Number,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:8
+    },
+    confirmPassword:{
+        type:String,
+        required:true,
+        minlength:8,
+        validate:function(){
+            return(this.password==this.confirmPassword)
+        }
+    }
+    // ,tokens:[
+    //     {token:{
+    //         type:String,
+    //         require:true
+    //     }
+    // }
+    // ]
+})
+
+userSchema.pre("save",async function(){
+    this.confirmPassword=undefined;
+        let salt=await bcrypt.genSalt()
+        const hash=bcrypt.hashSync(this.password, salt);
+        this.password=hash;
+})
+
+const paymentSchema=mongoose.Schema({
+    cardNo:{
+        type:String,
+        require:true
+    },
+    holderName:{
+        type:String,
+        require:true
+    },
+    mm:{
+        type:String,
+        require:true
+    },
+    yy:{
+        type:String,
+        require:true
+    },
+    cvv:{
+        type:String,
+        require:true
+    }
+})
+
+const checkoutSchema=mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        validate:function(){
+            return(emailValidator.validate(this.email))
+        }
+    },
+    fname:{
+        type:String,
+        required:true,
+    },
+    phone:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true,
+    },
+    zipcode:{
+        type:String,
+        required:true,
+        minlength:6
+    },
+    start:{
+        type:String,
+        required:true,
+    },
+    end:{
+        type:String,
+        required:true,
+    },
+    Cost:{
+        type:Number,
+        required:true,
+    }
+})
+
+const contactSchema=mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:false,
+        validate:function(){
+            return(emailValidator.validate(this.email))
+        }
+    },
+    phone:{
+        type:Number,
+        required:true,
+    },
+    message:{
+        type:String,
+        required:true
+    }
+})
+
+
+const contactModel=mongoose.model("contactModel",contactSchema)
+const checkoutModel=mongoose.model("checkoutModel",checkoutSchema)
+const paymentModel=mongoose.model("paymentModel",paymentSchema)
+const userModel=mongoose.model("userModel",userSchema)
+
+
+
+
+const userRouter=express.Router();
+app.use("/users",userRouter)
+
+userRouter
+.route("/register")
+.post(signup)
+
+
+async function signup(req,res){
+    try{
+    const {name,email,phone,password,confirmPassword}=req.body;
+    const user =await userModel.create({name,email,phone,password,confirmPassword});
+    if(user){
+        res.status(200).json({
+            message:"success",
+            status:200,
+            data:user
+        })
+    }
+}catch(e){
+    res.json({
+        message:e.message
+    })
+}
+}
+
+
+userRouter
+.route("/signin")
+.post(login)
+
+async function login(req,res){
+    try{
+    if(req.body.email){
+    let user= await userModel.findOne({email:req.body.email});
+    if(user){
+         bcrypt.compare(req.body.password, user.password, function(err, result) {
+            if(result){
+                const uid=user._id;
+                const token= jwt.sign({payload:uid},JWT_KEY)
+                res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+                res.header('Access-Control-Allow-Credentials', true);
+                res.cookie("login", token, { 
+                    maxAge: 1000*60*60*24*30,
+                    httpOnly: true,
+                    sameSite: "none",
+                    secure: false
+                  });
+                  
+                  
+                res.status(200).json({
+                    message:"user logged in successfully",
+                    status:200,
+                    token:token
+                })
+
+            }
+            if(err){
+                res.status(400).json({
+                    message:"invalid credentials"
+                })
+            }
+        });
+    }else{
+        res.status(401).json({
+            message:"user not found"
+        })
+    }
+}else{
+    res.status(300).json({
+        message:"please fill the empty fields"
+    })
+}
+    }
+    catch(e){
+        res.json({
+            message:e.message
+        })
+    }
+
+}
+userRouter
+.route("/contactus")
+.post(contactus)
+
+async function contactus(req,res){
+    try{
+        if((req.body.email || req.body.phone) && req.body.message){
+            const message=await contactModel.create(req.body)
+            if(message){
+                res.json({
+                    message:"message sent successfully",
+                    status:200
+                })
+            }
+        }
+
+    }catch(e){
+        res.json({
+            message:e.message,
+            status:e.status
+        })
+    }
+}
+
+app.get("/",function(req,res){
+    res.send("hello from backend")
+})
+
+userRouter.route("/payment")
+.post(payment)
+
+async function payment(req,res){
+    try{
+    if(req.body){
+        const data=await paymentModel.create(req.body);
+        if(data){
+            res.json({
+                message:"Payment Successfull",
+                status:200
+            })
+        }else{
+            res.json({
+                message:"unsucessfull payment",
+                status:404
+            })
+        }
+    }
+    else{
+        res.json({
+            message:"Invalid Credentials"
+        })
+    }
+
+}catch(e){
+    res.json({
+        message:e.message
+    })
+}
+}
+
+
+userRouter.route("/checkout")
+.post(checkout)
+
+async function checkout(req,res){
+    try{
+    if(req.body){
+        const data=await checkoutModel.create(req.body);
+        if(data){
+            res.json({
+                message:"checkout Successfull",
+                status:200
+            })
+        }else{
+            res.json({
+                message:"checkout unsuccessfull",
+                status:404
+            })
+        }
+    }
+    else{
+        res.json({
+            message:"Invalid Credentials"
+        })
+    }
+
+}catch(e){
+    res.json({
+        message:e.message
+    })
+}
+}
+
+
+
+app.listen(3010,()=>{console.log(`listening on port localhost:${3010}`)})

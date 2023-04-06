9bb20d9a32be9057ae686a56898e718747dcead8
Gzie3009 <108942030+Gzie3009@users.noreply.github.com>
2023-04-07 01:21:39 +0530

    cors update 1

--- app.js
+++ app.js
@@ -11,12 +11,19 @@ const dotenv=require("dotenv");
 dotenv.config({path: "./config.env"});
 const app= express()
 
-app.use(cors())
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://642f205340eb6907f078f1a8--chipper-manatee-b6069f.netlify.app/");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+const allowedOrigins = ['https://642f205340eb6907f078f1a8--chipper-manatee-b6069f.netlify.app'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
   
   app.use(express.json())
   app.use(bodyParser.urlencoded({ extended: true }));

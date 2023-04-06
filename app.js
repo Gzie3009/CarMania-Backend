92feb699ede505afb28bf998a2fc8d8432352cbc
Gzie3009 <108942030+Gzie3009@users.noreply.github.com>
2023-04-07 01:14:19 +0530

    allowing my frontend

--- app.js
+++ app.js
@@ -12,10 +12,12 @@ dotenv.config({path: "./config.env"});
 const app= express()
 
 app.use(cors())
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://642f205340eb6907f078f1a8--chipper-manatee-b6069f.netlify.app/");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
+  
   app.use(express.json())
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(cookieParser());

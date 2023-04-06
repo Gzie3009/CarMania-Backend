fab6f1f24ce1bd04bef3f61d62aabad4379582b6
Gzie3009 <108942030+Gzie3009@users.noreply.github.com>
2023-04-07 01:38:05 +0530

    cors 6

--- app.js
+++ app.js
@@ -26,6 +26,17 @@ const corsOptions = {
   };
   
   app.use(cors(corsOptions));
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://642f205340eb6907f078f1a8--chipper-manatee-b6069f.netlify.app");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
   app.use(express.json())
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(cookieParser());
